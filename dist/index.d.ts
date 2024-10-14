38e4e7c03f6abd5777cfef070955d0753635f5c6
mahmoud hussien <smartcaeser@gmail.com>
2024-10-14 10:17:47 +0300

    add ar

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import * as momenttimezone from 'moment-timezone';
+import "moment/locale/ar";
 export declare function fromNow(date: Date | string | number): string;
 export declare function toNow(date: Date | string | number): string;
 export { moment, momenttimezone };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import * as momenttimezone from 'moment-timezone';
-
+import "moment/locale/ar";
 
 export function fromNow(date: Date | string | number): string {
     momenttimezone.tz.setDefault('Africa/Cairo');
@@ -8,8 +8,9 @@ export function fromNow(date: Date | string | number): string {
 }
 
 export function toNow(date: Date | string | number): string {
-    return moment(date).toNow();
+    return moment(date).locale('ar').toNow();
 }
 
 // Expose moment for users
 export { moment, momenttimezone };
+
