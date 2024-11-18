b8ae95d3bfe6c36e1e6e001e45b28013809f79d0
mahmoud hussien <smartcaeser@gmail.com>
2024-11-18 14:12:43 +0200

    update swappmoment to load arabic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import * as momenttimezone from 'moment-timezone';
+import 'moment/locale/ar';
 
 
 export function fromNow(value: Date | string | number, locale: string): string {
